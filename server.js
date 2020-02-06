b928e9ce4e4158325d4cfd166f408432f69f2dd4
Victor Dominguez <vd071090@gmail.com>
2020-02-06 17:19:56 -0500

    updated models js files and made get request for website

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const axios = require("axios");
-const cherrio = require("cheerio");
+const cheerio = require("cheerio");
 
 const db = require("./models");
 
@@ -16,18 +16,62 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-app.set("views", "./views");
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
-app.set("view engine", "handlebars");
+// app.set("views", "./views");
+// app.engine(
+//   "handlebars",
+//   exphbs({
+//     defaultLayout: "main"
+//   })
+// );
+// app.set("view engine", "handlebars");
 
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI);
+
+/// A GET route for scraping bleacher report
+app.get("/scrape", function(req, res) {
+  axios.get("https://bleacherreport.com/").then(function(response) {
+    const $ = cheerio.load(response.data);
+
+    $(".articleTitle").each(function(i, element) {
+      let result = {};
+
+      result.title = $(this)
+      .children("h3")
+      .text();
+      result.link = $(this)
+      .children("a")
+      .attr("href");
+
+
+      // db.Article.create(result)
+      //   .then(function(dbArticle) {
+      //     console.log(dbArticle);
+      //   })
+      //   .catch(function(err) {
+      //     console.log(err);
+      //   });
+      res.json(result);
+    });
+
+    
+
+
+  });
+});
+
+
+// app.get("/articles", function(req, res) {
+//   db.Article.find({})
+//     .then(function(dbArticle) {
+//       res.json(dbArticle);
+//     })
+//     .catch(function(err) {
+//       res.json(err)
+//     })
+// })
+
 
 
 
